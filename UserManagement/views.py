9e99e723c40e524c97769c0bd6bc7bd28b07ae80
abdullahshinwari <abshinwari786@gmail.com>
2021-08-12 11:32:00 +0500

    changes in response for login and logout

--- UserManagement/views.py
+++ UserManagement/views.py
@@ -30,18 +30,20 @@ def login_user(request):
     image = request.FILES['image']
     try:
         account = Users.objects.get(username=username)
-    except BaseException as e:
-        return Response(response_body("", "error", str(e), 400))
+    except :
+        return Response(response_body("", "error", "incorrect username", 400))
+
+    if not check_password(password, account.password):
+        return Response(response_body("", "error", "incorrect password", 400))
+    if not face_recognize(account.profile_pic.url, image):
+        return Response(response_body("", "error", "image doesn't match", 400))
     try:
         token, _ = Token.objects.get_or_create(user=account)
         user_token = token.key
     except BaseException as e:
         return Response(response_body("", "error", str(e), 400))
 
-    if not check_password(password, account.password):
-        return Response(response_body("", "error", "incorrect login credentials", 400))
-
-    if account and account.is_active and not account.is_deleted and face_recognize(account.profile_pic.url, image):
+    if account and account.is_active and not account.is_deleted:
         login(request, account)
         return Response(response_body({"token": user_token}, "success", "user logged in successfully", 1000))
     else:
@@ -56,7 +58,7 @@ def logout_user(request):
         request.user.auth_token.delete()
         logout(request)
     except BaseException as e:
-        return Response(response_body("", "error", e, 400))
+        return Response(response_body("", "error", str(e), 400))
     return Response(response_body("", "success", "user logout successfully", 1000))
 
 
